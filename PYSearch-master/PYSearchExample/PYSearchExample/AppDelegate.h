51ceb30c2b6357df87c5baa5061e10e191275769
TroubleisFriend <ding_tk@163.com>
2017-07-07 16:56:01 +0800

    搜索页面历史记录热门搜索

---
+++ PYSearch-master/PYSearchExample/PYSearchExample/AppDelegate.h
@@ -0,0 +1,16 @@
+// 
+//  代码地址: https://github.com/iphone5solo/PYSearch
+//  代码地址: http://www.code4app.com/thread-11175-1-1.html
+//  Created by CoderKo1o.
+//  Copyright © 2016年 iphone5solo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
